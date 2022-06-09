376af5655fd425d241b498eebda2c07b9c3982e2
Skueletor <73276191+SkueletorTlg@users.noreply.github.com>
2022-06-08 20:38:53 -0400

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -30,15 +30,15 @@ async def media_receive_handler(_, m: Message):
     log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
     stream_link = f"{Var.URL}{log_msg.message_id}/{quote_plus(get_name(m))}?hash={get_hash(log_msg)}"
     short_link = f"{Var.URL}{get_hash(log_msg)}{log_msg.message_id}"
-    logging.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
+    logging.info(f"Enlace generado: {stream_link} de {m.from_user.first_name}")
     rm = InlineKeyboardMarkup(
-        [[InlineKeyboardButton("Open", url=stream_link)]]
+        [[InlineKeyboardButton("Abrir", url=stream_link)]]
     )
     if Var.FQDN == Var.BIND_ADDRESS:
         # dkabl
         rm = None
     await m.reply_text(
-        text="<code>{}</code>\n(<a href='{}'>shortened</a>)".format(
+        text="<code>{}</code>\n(<a href='{}'>acortado</a>)".format(
             stream_link, short_link
         ),
         quote=True,
