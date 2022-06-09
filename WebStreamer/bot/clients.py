4163bac1211de8e3747d5ef1c8a64e2c4b904c69
Skueletor <73276191+SkueletorTlg@users.noreply.github.com>
2022-06-08 20:44:23 -0400

    Update clients.py

--- WebStreamer/bot/clients.py
+++ WebStreamer/bot/clients.py
@@ -14,7 +14,7 @@ async def initialize_clients():
     work_loads[0] = 0
     all_tokens = TokenParser().parse_from_env()
     if not all_tokens:
-        print("No additional clients found, using default client")
+        print("No se encontraron clientes adicionales, utilizando el cliente predeterminado")
         return
     
     async def start_client(client_id, token):
@@ -22,9 +22,9 @@ async def initialize_clients():
             print(f"Starting - Client {client_id}")
             if client_id == len(all_tokens):
                 await asyncio.sleep(2)
-                print("This will take some time, please wait...")
+                print("Esto tomará algún tiempo, por favor espere...")
             client = await Client(
-                session_name=":memory:",
+                session_name=":memoria:",
                 api_id=Var.API_ID,
                 api_hash=Var.API_HASH,
                 bot_token=token,
@@ -34,12 +34,12 @@ async def initialize_clients():
             work_loads[client_id] = 0
             return client_id, client
         except Exception:
-            logging.error(f"Failed starting Client - {client_id} Error:", exc_info=True)
+            logging.error(f"error al iniciar el cliente - {client_id} Error:", exc_info=True)
     
     clients = await asyncio.gather(*[start_client(i, token) for i, token in all_tokens.items()])
     multi_clients.update(dict(clients))
     if len(multi_clients) != 1:
         Var.MULTI_CLIENT = True
-        print("Multi-Client Mode Enabled")
+        print("Modo Multi-Client habilitado")
     else:
-        print("No additional clients were initialized, using default client")
+        print("No se inicializaron clientes adicionales, utilizando el cliente predeterminado")
